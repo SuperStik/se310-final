396b4a4333850e982ddd0d382bc420ec64bf7a55
SuperStik <daviswood290@gmail.com>
2025-12-03 16:24:13 -0800

    Add repository patterns

--- src/main/java/com/se310/store/repository/StoreRepository.java
+++ src/main/java/com/se310/store/repository/StoreRepository.java
@@ -3,6 +3,8 @@ package com.se310.store.repository;
 import com.se310.store.data.DataManager;
 import com.se310.store.model.Store;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.*;
 
 /**
@@ -18,12 +20,25 @@ import java.util.*;
  */
 public class StoreRepository {
 
-    //TODO: Implement Store persistence layer using Repository Pattern
-
     private final DataManager dataManager;
 
     public StoreRepository(DataManager dataManager) {
-
         this.dataManager = dataManager;
+    }
+
+    public void addStore(Store store) throws SQLException {
+        dataManager.executeUpdate("INSERT INTO stores (id, address, description) VALUES (?, ?, ?)", store.getId(),
+                store.getAddress(), store.getDescription());
+    }
+
+    public void removeStore(String id) throws SQLException {
+        dataManager.executeUpdate("DELETE FROM stores WHERE id = ?", id);
+    }
+
+    public Store findStore(String id) throws SQLException {
+        ResultSet result = dataManager.executeQuery("SELECT id, address, description FROM stores WHERE id = ?",
+                id);
+        return new Store(result.getString(0), result.getString(1),
+                result.getString(2));
     }
 }
--- src/main/java/com/se310/store/repository/UserRepository.java
+++ src/main/java/com/se310/store/repository/UserRepository.java
@@ -1,8 +1,12 @@
 package com.se310.store.repository;
 
 import com.se310.store.data.DataManager;
+import com.se310.store.model.Store;
 import com.se310.store.model.User;
+import com.se310.store.model.UserRole;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.*;
 
 /**
@@ -18,11 +22,25 @@ import java.util.*;
  */
 public class UserRepository {
 
-    //TODO: Implement User persistence layer using Repository Pattern
-
     private final DataManager dataManager;
 
     public UserRepository(DataManager dataManager) {
         this.dataManager = dataManager;
+    }
+
+    public void addUser(User user) throws SQLException {
+        dataManager.executeUpdate("INSERT INTO users (email, password, name, role) VALUES (?, ?, ?, ?)",
+                user.getEmail(), user.getPassword(), user.getName(), user.getRole());
+    }
+
+    public void removeUser(String email) throws SQLException {
+        dataManager.executeUpdate("DELETE FROM users WHERE email = ?", email);
+    }
+
+    public User findUser(String email) throws SQLException {
+        ResultSet result = dataManager.executeQuery("SELECT email, password, name, role FROM users WHERE email = ?",
+                email);
+        return new User(result.getString(0), result.getString(1),
+                result.getString(2), UserRole.valueOf(result.getString(3)));
     }
 }
